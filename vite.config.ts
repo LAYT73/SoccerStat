4f5613dce546ff21aecf103f0afe21a541944f81
Никита Шипилов <116575516+LAYT73@users.noreply.github.com>
2026-03-14 20:15:36 +0400

    chore: add bundle visualizer and ESLint checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import babel from '@rolldown/plugin-babel'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 
@@ -12,10 +13,20 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
     checker({
       typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,js,jsx}"',
+        useFlatConfig: true,
+      },
+    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'bundle_statistics.html',
     }),
   ],
 })
