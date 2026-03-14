6afcec8227fc5a7205bce2e8feda9711c79111d4
Никита Шипилов <116575516+LAYT73@users.noreply.github.com>
2026-03-14 20:00:59 +0400

    chore: add TS path alias; remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,20 @@ import babel from '@rolldown/plugin-babel'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [
-    tsconfigPaths(),
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     checker({
       typescript: true,
-      eslint: {
-        lintCommand: 'eslint .',
-      },
     }),
   ],
 })
